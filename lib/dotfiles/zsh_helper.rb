6978f32bd42ba16cbeaa0dcf8b18be18ca675599
Andrea Dal Ponte <info@andreadalponte.com>
2019-02-11 16:24:05 +0100

    Change: setup zsh

--- lib/dotfiles/zsh_helper.rb
+++ lib/dotfiles/zsh_helper.rb
@@ -1,20 +1,35 @@
 # frozen_string_literal: true
 
-require 'rake'
+require_relative './base_helper'
 
 module Dotfiles
   # Install Rake tasks for zsh
-  class ZshHelper
+  class ZshHelper < BaseHelper
     def install
       namespace :zsh do
         desc 'Setup zsh'
-        task install: %i[ohmyzsh] do
-          echo 'Zsh installed!'
+        task install: %i[ohmyzsh chsh link] do
+          puts 'Zsh installed!'
         end
 
         desc 'Setup Oh my zsh'
         task :ohmyzsh do
-          sh '$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)'
+          sh 'curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh | bash'
+        end
+
+        desc 'Set zsh as default shell'
+        task :chsh do
+          sh 'chsh -s $(which zsh)'
+        end
+
+        desc 'Link zprofile'
+        task :link do
+          dotfile = home('.zprofile')
+          if File.exist? dotfile
+            warn "#{dotfile} already exists"
+          else
+            ln_s File.join('.dotfiles', 'zshfiles', 'zprofile'), dotfile
+          end
         end
       end
     end
