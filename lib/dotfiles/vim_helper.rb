efe1068379c649957e699e9109b143a73793caf5
Andrea Dal Ponte <info@andreadalponte.com>
2019-02-11 12:15:53 +0100

    Change: add vim tasks

--- lib/dotfiles/vim_helper.rb
+++ lib/dotfiles/vim_helper.rb
@@ -15,10 +15,36 @@ module Dotfiles
     def install
       namespace :vim do
         desc 'Setup VIM'
-        task :install do
-          puts 'Hello vim!'
+        task install: %i[vundle] do
+          puts 'Vim installed!'
+        end
+
+        desc 'Install Vundle'
+        task :vundle do
+          if File.exist? home('.vim/bundle/Vundle.vim')
+            warn 'Vundle already installed'
+          else
+            mkdir_p home('.vim/bundle/')
+            sh 'git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim'
+          end
+        end
+
+        desc %(Make ~/.vimrc symlink)
+        task :link do
+          dotfile = home('.vimrc')
+          if File.exist? dotfile
+            warn "#{dotfile} already exists"
+          else
+            ln_s File.join('vimfiles', 'vimrc'), dotfile
+          end
         end
       end
+    end
+
+    private
+
+    def home(path = '')
+      File.join ENV['HOME'], path
     end
   end
 end
